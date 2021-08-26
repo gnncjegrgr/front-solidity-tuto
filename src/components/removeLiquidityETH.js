1962b5a543b604ed3d0e32f881cd4051744bb87e
skystar234 <80614306+skystar234@users.noreply.github.com>
2021-08-27 04:49:24 +0900

    Add files via upload

---
+++ src/components/removeLiquidityETH.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {useWeb3React} from '@web3-react/core'
+import {ROUTERR_ABI} from "../constant";
+import {ethers } from 'ethers';
+
+function RemoveLiquidityETH(){
+    const {account, library} = useWeb3React()
+    const signer = library?.getSigner(account).connectUnchecked()
+    const routerContract = new ethers.Contract('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', ROUTERR_ABI, signer)
+    const onClick = () => {
+        routerContract.removeLiquidity(
+            'tokenA',
+            'tokenB',
+            ethers.utils.parseEther("0.1"),
+            '0',
+            '0',
+            '0x739ECe608f49Ed10828e9B14b57D4adc1D8EB276',
+            Math.floor(Date.now() / 1000) + 60 * 20,
+            { gasLimit: ethers.utils.hexlify(250000), gasPrice: ethers.utils.parseUnits('5', "gwei")}
+        ).then(result => console.log(result))
+    }
+
+    return (
+        <div>
+            <button onClick = {onClick}>
+                Remove Liquidity ETH
+            </button>
+        </div>
+    )
+}
+
+export default RemoveLiquidityETH
